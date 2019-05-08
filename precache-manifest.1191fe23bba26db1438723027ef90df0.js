9427d20693ecc61ff7568a820759b692588288e8
sankaraasn <sankaraasn@gmail.com>
2019-05-08 22:26:45 +0530

    Updates

---
+++ precache-manifest.1191fe23bba26db1438723027ef90df0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b24eb2a0fecb4baa009af14726ce813",
+    "url": "/bring-up/index.html"
+  },
+  {
+    "revision": "dcfaae7cd9accf84a1bc",
+    "url": "/bring-up/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/bring-up/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "dcfaae7cd9accf84a1bc",
+    "url": "/bring-up/static/js/main.35faa33d.chunk.js"
+  },
+  {
+    "revision": "5242f59f1b3655ddd732",
+    "url": "/bring-up/static/js/runtime~main.43faace0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bring-up/static/media/logo.5d5d9eef.svg"
+  }
+]);
