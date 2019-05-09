53110dbc651dfcf0b06608334f2db520d08728e9
sankaraasn <sankaraasn@gmail.com>
2019-05-10 01:00:10 +0530

    Icons added

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Editor = () => {
     return (
-        <div contentEditable={'plaintext-only'} className={"p-2 text-left __bu-content"}>
+        <div contentEditable={'plaintext-only'} className={"p-2 text-left __bu-content __bu-editor-border"}>
 
         </div>
     )
