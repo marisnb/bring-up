75fdb206c053e6b808d7889873fec086befdcf45
sankaraasn <sankaraasn@gmail.com>
2019-06-03 07:45:56 +0530

    Updates

---
+++ precache-manifest.94f0ecc47b87bca21dafc4482494601e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f6d15428ae4f92ea609b45328bb2f4",
+    "url": "/bring-up/index.html"
+  },
+  {
+    "revision": "0a5400d021148d507b3f",
+    "url": "/bring-up/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "abd22ec2cb203090ac7a",
+    "url": "/bring-up/static/css/main.f7ef7788.chunk.css"
+  },
+  {
+    "revision": "0a5400d021148d507b3f",
+    "url": "/bring-up/static/js/2.4259960b.chunk.js"
+  },
+  {
+    "revision": "abd22ec2cb203090ac7a",
+    "url": "/bring-up/static/js/main.7cdce764.chunk.js"
+  },
+  {
+    "revision": "5242f59f1b3655ddd732",
+    "url": "/bring-up/static/js/runtime~main.43faace0.js"
+  },
+  {
+    "revision": "407e00f009328c37055f67350d65ed8d",
+    "url": "/bring-up/static/media/boy.407e00f0.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bring-up/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.36819e2ffcab22a7dfe5a292af491ce3.js"
+  "/bring-up/precache-manifest.94f0ecc47b87bca21dafc4482494601e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bring-up/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
